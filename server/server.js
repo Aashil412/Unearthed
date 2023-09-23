65d68180be3f902688b99c56b048001d453a3ad6
Aashi <aashilahmad@gmail.com>
2023-09-23 15:10:53 -0400

    Unearthed

--- server/server.js
+++ server/server.js
@@ -1,15 +1,22 @@
-import express from "express";
+import express from 'express'
+
+import './config/dotenv.js'
+
 import giftsRouter from './routes/gifts.js'
+
 const app = express()
 
-app.use('/public',express.static('./public'))
+app.use('/public', express.static('./public'))
 app.use('/scripts', express.static('./public/scripts'))
-app.get('/', (req, res) => {
-    res.status(200).send('<h1 style="text-align: center; margin-top: 50px;">UnEarthed API</h1>')
-  })
-  
-app.use('/gifts',giftsRouter)
 
-app.listen(3001, () => {
-    console.log('Server listening on http://localhost:3001')
+app.use('/gifts', giftsRouter)
+
+app.get('/', (req, res) => {
+  res.status(200).send('<h1 style="text-align: center; margin-top: 50px;">UnEarthed API</h1>')
+})
+
+const PORT = process.env.PORT || 3001
+    
+app.listen(PORT, () => {
+  console.log(`🚀 Server listening on http://localhost:${PORT}`)
 })
